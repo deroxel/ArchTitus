e63d0fc8559ea94f56a4c224bf6289007a16c5a2
Egils Eglītis <40385963+deroxel@users.noreply.github.com>
2021-11-23 17:17:37 +0200

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -251,3 +251,25 @@ else
 	echo "You are already a user proceed with aur installs"
 fi
 
+echo -e "\nINSTALLING AUR SOFTWARE\n"
+# You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
+
+echo "CLONING: YAY"
+cd ~
+git clone "https://aur.archlinux.org/yay.git"
+cd ${HOME}/yay
+makepkg -si --noconfirm
+cd ~
+touch "$HOME/.cache/zshhistory"
+git clone "https://github.com/ChrisTitusTech/zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
+
+export PATH=$PATH:~/.local/bin
+cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
+pip install konsave
+konsave -i $HOME/ArchTitus/kde.knsv
+sleep 1
+konsave -a kde
+
+echo -e "\nDone!\n"
